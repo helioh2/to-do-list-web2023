cbb19aa6d427226c9b57602393d2e85acde92721
Helio <heliohenrique3@gmail.com>
2023-04-06 12:04:27 -0300

    Criação da estrutura/arquitetura dos arquivos do projeto. Código inicial do app.js,

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+
+// ROTA
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
+})
