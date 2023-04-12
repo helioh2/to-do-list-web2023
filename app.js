07a239206f672bfacae87bbefb14d8afd6ac6e2c
Helio <heliohenrique3@gmail.com>
2023-04-12 10:35:39 -0300

    configuração inicial do index (app.js), routes e controller

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
+const path = require('path');
+const routes = require("./routes/routes")
 
 const app = express()
-const port = 3000
+const port = 3000;  //será alterado por meio de variáveis de ambiente depois 
 
-
-// ROTA
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
