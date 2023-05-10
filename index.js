c68d92113771714e096277e0c3532558848c881e
Helio <heliohenrique3@gmail.com>
2023-05-10 14:34:24 -0300

    Teste cookie

--- index.js
+++ index.js
@@ -31,6 +31,35 @@ app.use(function(req, res, next) {
 });
 
 
+app.get("/test_set_cookie", (req, res) => {
+  let cookie = req.cookies.bulacha;
+  if (cookie === undefined) {
+    // no: set a new cookie
+    let randomNumber = Math.random().toString();
+    randomNumber = randomNumber.substring(2,randomNumber.length);
+    res.cookie('bulacha',randomNumber, { maxAge: 900000, httpOnly: true });
+    console.log('cookie criado');
+    res.send("COOKIE CRIADO");
+  } else {
+    // yes, cookie was already present 
+    console.log('cookie já existe', cookie);
+    res.send("COOKIE JÁ EXISTE");
+  } 
+})
+
+app.get("/test_get_cookie", (req, res) => {
+  let cookie = req.cookies.bulacha;
+  if (cookie === undefined) {
+    res.send("COOKIE NÃO ENCONTRADO!!");
+  } else {
+    // yes, cookie was already present 
+    console.log('cookie exists', cookie);
+    res.send(`VALOR DO COOKIE: ${cookie}`);
+  } 
+})
+
+
+
 app.use(routes)
 
 app.listen(port, () => {
